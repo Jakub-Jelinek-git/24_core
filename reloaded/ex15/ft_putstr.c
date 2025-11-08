88d6cd58a0eab826c38b788550b401f5e91e7135
Jakub Jelinek <jakub2007@atlas.cz>
2025-11-08 16:09:32 +0100

    ex15

---
+++ reloaded/ex15/ft_putstr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjelinek <jjelinek@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 16:02:52 by jjelinek          #+#    #+#             */
+/*   Updated: 2025/11/08 16:09:03 by jjelinek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+/*int main(void)
+{
+	char str[] = "dwedwdijwoidjwoijd§oiawjd§ůioj";
+	ft_putstr(str);
+	return 0;
+}*/
