8ce0bc18db267fccdd69c452f81f2cbdfcb127f4
Jakub Jelinek <jakub2007@atlas.cz>
2025-11-20 12:29:58 +0100

    small degugging on calloc and putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jjelinek <jjelinek@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 10:15:42 by jjelinek          #+#    #+#             */
-/*   Updated: 2025/11/19 10:40:04 by jjelinek         ###   ########.fr       */
+/*   Updated: 2025/11/19 18:45:59 by jjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	ft_putnbr_fd(int n, int fd)
 	if (nb < 0)
 	{
 		write(fd, "-", 1);
-		nb *= -1;
+		nb = -nb;
 	}
-	if (nb > 10)
+	if (nb >= 10)
 		ft_putnbr_fd(nb / 10, fd);
 	ft_putchar_fd('0' + (nb % 10), fd);
 }
