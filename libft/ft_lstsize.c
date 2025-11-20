7ab55da80299601062fb5af334733621e0ee7af6
Jakub Jelinek <jakub2007@atlas.cz>
2025-11-20 13:38:43 +0100

    added ě new list manipulation function

---
+++ libft/ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjelinek <jjelinek@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 13:31:48 by jjelinek          #+#    #+#             */
+/*   Updated: 2025/11/20 13:36:41 by jjelinek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+	t_list	*current;
+	int	len;
+
+	len = 0;
+	current = lst;
+	while(current != NULL)
+	{
+		current = current->next;
+		len++;
+	}
+	return (len);
+}
